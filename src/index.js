b0bf58d3d61d9f13e628480e94999749d217b5d0
Christopher Kramer <Hendrix1957@gmail.com>
2017-04-25 21:45:17 -0400

    DOM Practice

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.querySelector('h1').textContent += ''
 }
 
+var el = document.createElement('button')
+document.body.appendChild(el)
+el.className = 'container'
+el.style.marginTop = '300px'
+el.style.width = '300px'
+el.style.height = '40px'
+el.style.color = '#ff30'
+
+var element = document.createElement('canvas')
+document.getElementsByClassName(element)
+element.className = 'canvas'
+el.style.width = '300px'
+el.style.color = '#ee33'
+
+console.log(element)
 document.addEventListener('DOMContentLoaded', main)
 
 // HERE BE DRAGONS... and webpack. Don't touch.
